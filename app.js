ff8970123a39844fe77a34c64e5e487c017c40cf
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-08-07 14:50:10 -0700

    navbar animation effects

---
+++ app.js
@@ -0,0 +1,7 @@
+openNavbar = () => {
+    document.getElementById('open_nav_div').style.display = "block"
+}
+
+closeNavbar = () => {
+    document.getElementById('open_nav_div').style.display = "none"
+}
