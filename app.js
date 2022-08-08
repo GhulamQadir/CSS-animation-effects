bed5abea6250a92f3ba2255f4357999089e4efd7
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-08-08 06:59:25 -0700

    animating div

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 openNavbar = () => {
-    document.getElementById('open_nav_div').style.display = "block"
+    let z = document.getElementById('open_nav_div')
+    z.classList.remove('hide_div')
+    z.style.display = "block"
+
 }
 
 closeNavbar = () => {
     let a = document.getElementById('open_nav_div')
     a.className += ' hide_div'
 }
+
+
+
+$(document).ready(function () {
+    $(".slide-toggle").click(function () {
+        $(".box").animate({
+            width: "toggle"
+        });
+    });
+});
